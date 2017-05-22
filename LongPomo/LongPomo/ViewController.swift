972a47a5f74916ed8f090ae5bfffae24481ad966
Daniel Vela <dani_vela@me.com>
2017-05-22 18:34:33 +0200

    removed font awesome

--- LongPomo/LongPomo/ViewController.swift
+++ LongPomo/LongPomo/ViewController.swift
@@ -10,13 +10,22 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var mainButton: NSButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureAppearance()
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+
+    func configureAppearance() {
+    }
+
+    @IBAction func mainButtonTapped(_ sender: Any) {
     }
 }
