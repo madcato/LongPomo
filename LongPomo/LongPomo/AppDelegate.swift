8ebac8f433aac4212ea99deb77999b84368002ab
Daniel Vela <dani_vela@me.com>
2017-05-22 17:53:59 +0200

    Added main colours App background set to red

--- LongPomo/LongPomo/AppDelegate.swift
+++ LongPomo/LongPomo/AppDelegate.swift
@@ -14,13 +14,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        configureMainAppearance()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
 
-
+    func configureMainAppearance() {
+        let window = NSApplication.shared().mainWindow
+        window?.titlebarAppearsTransparent = true
+        window?.backgroundColor = NSColor.black
+    }
 }
 
