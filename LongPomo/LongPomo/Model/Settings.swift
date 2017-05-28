45a61015693da35c78e4f8e722ad4c6db5cccddc
Daniel Vela <dani_vela@me.com>
2017-05-28 10:31:32 +0200

    Added simple presistence to store settings

--- LongPomo/LongPomo/Model/Settings.swift
+++ LongPomo/LongPomo/Model/Settings.swift
@@ -9,10 +9,31 @@
 import Foundation
 
 class Settings {
+    static var kPomodoInSeconds: String = "kPomodoInSeconds"
+    static var kRestingInSeconds: String = "kRestingInSeconds"
     static var pomodoroInSeconds: Double = {
-        return 0.1.minutes()  // 90 minutes
-    }()
+        let seconds = SimplePersistence.double(forKey: kPomodoInSeconds)
+        if seconds != 0 {
+            return seconds
+        } else {
+            return 90.minutes()  // 90 minutes
+        }
+    }() {
+        didSet {
+            SimplePersistence.store(pomodoroInSeconds, forKey: kPomodoInSeconds)
+        }
+    }
+
     static var restingInSeconds: Double = {
-        return 0.05.minutes()  // 20 minutes
-    }()
+        let seconds = SimplePersistence.double(forKey: kRestingInSeconds)
+        if seconds != 0 {
+            return seconds
+        } else {
+            return 20.minutes()  // 20 minutes
+        }
+        }() {
+        didSet {
+            SimplePersistence.store(pomodoroInSeconds, forKey: kRestingInSeconds)
+        }
+    }
 }
