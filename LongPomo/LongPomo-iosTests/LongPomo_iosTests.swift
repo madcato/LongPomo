4b214b90230492e128bcbe60b8b599d3177c0e44
Daniel Vela <danielvela@maz.es>
2018-02-08 11:23:43 +0100

    ios Target and basic ui with flip transition

---
+++ LongPomo/LongPomo-iosTests/LongPomo_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  LongPomo_iosTests.swift
+//  LongPomo-iosTests
+//
+//  Created by Daniel Vela on 08/02/2018.
+//  Copyright © 2018 Daniel Vela. All rights reserved.
+//
+
+import XCTest
+@testable import LongPomo_ios
+
+class LongPomo_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
