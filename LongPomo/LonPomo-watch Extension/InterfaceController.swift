967a85657a77f94dd9ddda0cee18162ac500ca95
Daniel Vela <danielvela@maz.es>
2018-02-12 08:54:11 +0100

    Added WatchKit extension

---
+++ LongPomo/LonPomo-watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  LonPomo-watch Extension
+//
+//  Created by Daniel Vela on 12/02/2018.
+//  Copyright © 2018 Daniel Vela. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
